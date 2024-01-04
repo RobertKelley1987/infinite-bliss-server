60383031875812feb3ae2fa2ed404c670d09f64e
Robert Kelley <77930488+RobertKelley1987@users.noreply.github.com>
2024-01-04 12:57:00 -0500

    Delete js-backup-data directory
    Forgot to add this folder to .gitignore on previous commits

--- js-backup-data/index.js
+++
@@ -1,5 +0,0 @@
-const { apparel } = require('./apparel');
-const { albums } = require('./albums');
-const { accessories } = require('./accessories');
-
-module.exports.data = [...apparel, ...albums, ...accessories];
