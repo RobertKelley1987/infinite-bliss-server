51a2e55f52e0067e4487798dfc1e5b7e6d75eb6d
Robert Kelley <robertkelley1987@gmail.com>
2024-01-04 12:11:42 -0500

    Updated server to print port number when listening.

--- server.js
+++ server.js
@@ -1,41 +1,44 @@
-if(process.env.NODE_ENV !== 'production') {
-    require('dotenv').config();
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
 }
 
 // Import required vars
-const express = require('express');
+const express = require("express");
 const app = express();
-const path = require('path');
-const cors = require('cors');
+const path = require("path");
+const cors = require("cors");
 
-const apparelRoutes = require('./routes/apparel');
-const accessoryRoutes = require('./routes/accessories');
-const musicRoutes = require('./routes/music');
-const artistRoutes = require('./routes/artists');
-const orderRoutes = require('./routes/orders');
-const paymentRoutes = require('./routes/payment-intents');
+const apparelRoutes = require("./routes/apparel");
+const accessoryRoutes = require("./routes/accessories");
+const musicRoutes = require("./routes/music");
+const artistRoutes = require("./routes/artists");
+const orderRoutes = require("./routes/orders");
+const paymentRoutes = require("./routes/payment-intents");
 
 // General config
 app.use(express.json());
-app.use(express.static('build'));
-app.use(cors({
-    origin: 'https://infinite-bliss-client.onrender.com',
-    credentials: true
-}));
+app.use(express.static("build"));
+app.use(
+  cors({
+    origin: "https://infinite-bliss-client.onrender.com",
+    credentials: true,
+  })
+);
 
 // Routing config
-app.use('/apparel', apparelRoutes);
-app.use('/accessories', accessoryRoutes);
-app.use('/music', musicRoutes);
-app.use('/artists', artistRoutes);
-app.use('/orders', orderRoutes);
-app.use('/payment-intents', paymentRoutes);
+app.use("/apparel", apparelRoutes);
+app.use("/accessories", accessoryRoutes);
+app.use("/music", musicRoutes);
+app.use("/artists", artistRoutes);
+app.use("/orders", orderRoutes);
+app.use("/payment-intents", paymentRoutes);
 
 // Error handling
 app.use((error, req, res, next) => {
-    console.log(error);
-    return res.status(error.status).send({ errorMessage: error.message })
+  console.log(error);
+  return res.status(error.status).send({ errorMessage: error.message });
 });
 
 // Start serving
-app.listen(process.env.PORT || 8080, () => console.log('The server listens...'));
+const port = process.env.PORT || 8080;
+app.listen(port, () => console.log(`The server is listening on port ${port}`));
